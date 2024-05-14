90dba3f3fbbdc499698cb16f09eeea1ff0df9e04
HeyMaau <841294180@163.com>
2024-05-15 00:41:45 +0800

    feat：适配底部小横条和状态栏

--- app/src/main/java/top/manpok/blog/activity/MainActivity.kt
+++ app/src/main/java/top/manpok/blog/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package top.manpok.blog.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,6 +13,7 @@ import top.manpok.blog.ui.theme.BlogSystemAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             BlogSystemAndroidTheme {

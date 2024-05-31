1e5ebd44b7c17b7652c9f39c7722d6183d12f15c
manpok <841294180@163.com>
2024-05-31 14:13:26 +0800

    feat：文章分类页布局调整，重构下拉菜单，去除popup

--- app/src/main/java/top/manpok/blog/component/CategoryPopup.kt
+++ app/src/main/java/top/manpok/blog/component/CategoryPopup.kt
@@ -1,27 +1,52 @@
 package top.manpok.blog.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupProperties
+import androidx.compose.ui.unit.dp
+import top.manpok.blog.pojo.BlogCategory
+import top.manpok.blog.utils.Constants
 
 @Composable
 fun CategoryPopup(
-    show: Boolean,
+    dataList: List<BlogCategory>,
     yOffset: Int = 0,
-    dismissRequest: () -> Unit,
-    content: @Composable () -> Unit
+    onClick: (index: Int) -> Unit
 ) {
-    if (show) {
-        Popup(
-            offset = IntOffset(0, yOffset),
-            onDismissRequest = dismissRequest,
-            properties = PopupProperties(
-                focusable = true
+    Box(
+        contentAlignment = Alignment.TopCenter, modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(0.dp, 0.dp, 15.dp, 15.dp)
             )
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start
         ) {
-            content()
+            dataList.forEachIndexed { index, blogCategory ->
+                CategoryListItem(
+                    coverUrl = Constants.BASE_IMAGE_URL + blogCategory.cover,
+                    name = blogCategory.name!!,
+                    isFirst = index == 0,
+                    isLast = index == dataList.size - 1,
+                    click = {
+                        onClick?.invoke(index)
+                    }
+                )
+                if (index != dataList.size - 1) {
+                    HorizontalDivider(modifier = Modifier.width(200.dp))
+                }
+            }
         }
     }
 }
--- app/src/main/java/top/manpok/blog/page/CategoryPage.kt
+++ app/src/main/java/top/manpok/blog/page/CategoryPage.kt
@@ -1,7 +1,6 @@
 package top.manpok.blog.page
 
 import android.content.Intent
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -11,15 +10,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -37,6 +32,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -46,7 +42,6 @@ import top.manpok.blog.R
 import top.manpok.blog.activity.ArticleDetailActivity
 import top.manpok.blog.component.ArticleListItem
 import top.manpok.blog.component.CategoryInfoCard
-import top.manpok.blog.component.CategoryListItem
 import top.manpok.blog.component.CategoryPopup
 import top.manpok.blog.component.CommonHeader
 import top.manpok.blog.utils.Constants
@@ -54,7 +49,7 @@ import top.manpok.blog.viewmodel.ArticleCategoryViewModel
 import top.manpok.blog.viewmodel.CategoryViewModel
 import top.manpok.blog.viewmodel.UserViewModel
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CategoryPage(
     modifier: Modifier = Modifier,
@@ -77,6 +72,8 @@ fun CategoryPage(
     }
     DisposableEffect(key1 = categoryViewModel.currentIndex) {
         if (categoryViewModel.currentIndex != -1) {
+            articleCategoryViewModel.currentPage = Constants.DEFAULT_PAGE
+            articleCategoryViewModel.pageSize = Constants.DEFAULT_PAGE_SIZE
             articleCategoryViewModel.getArticleListByCategory(
                 articleCategoryViewModel.currentPage,
                 articleCategoryViewModel.pageSize,
@@ -89,9 +86,10 @@ fun CategoryPage(
         mutableStateOf(false)
     }
     var commonHeaderHeight by remember {
-        mutableIntStateOf(0)
+        mutableStateOf(0.dp)
     }
     val context = LocalContext.current
+    val density = LocalDensity.current
     val pullRefreshState = rememberPullRefreshState(
         refreshing = articleCategoryViewModel.refreshing,
         onRefresh = {
@@ -104,79 +102,7 @@ fun CategoryPage(
         contentAlignment = Alignment.TopCenter,
         modifier = modifier.pullRefresh(state = pullRefreshState, enabled = true)
     ) {
-        LazyColumn() {
-            stickyHeader {
-                var headerHeight by remember {
-                    mutableIntStateOf(0)
-                }
-                CommonHeader(
-                    leftIcon = R.drawable.ic_arrow_back,
-                    rightIcon = R.drawable.ic_more,
-                    leftIconClick = { },
-                    rightIconClick = { },
-                    modifier = Modifier
-                        .background(Color.White)
-                        .padding(12.dp, 0.dp)
-                        .onGloballyPositioned {
-                            commonHeaderHeight = it.size.height
-                        }
-                ) {
-                    Box {
-                        Row(modifier = Modifier
-                            .onGloballyPositioned {
-                                headerHeight = it.size.height
-                            }
-                            .clickable {
-                                showCategoryPopup = !showCategoryPopup
-                            }) {
-                            Text(
-                                text = if (categoryViewModel.currentIndex != -1) categoryViewModel.categoryList[categoryViewModel.currentIndex].name!! else stringResource(
-                                    id = R.string.default_category_android
-                                )
-                            )
-                            Icon(
-                                imageVector = ImageVector.vectorResource(id = R.drawable.ic_arrow_drop_down),
-                                contentDescription = null
-                            )
-                        }
-                        CategoryPopup(
-                            show = showCategoryPopup,
-                            yOffset = headerHeight,
-                            dismissRequest = {
-                                showCategoryPopup = false
-                            }) {
-                            Box(
-                                contentAlignment = Alignment.TopCenter, modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(
-                                        color = Color.White,
-                                        shape = RoundedCornerShape(0.dp, 0.dp, 15.dp, 15.dp)
-                                    )
-                            ) {
-                                Column(
-                                    horizontalAlignment = Alignment.Start
-                                ) {
-                                    categoryViewModel.categoryList.forEachIndexed { index, blogCategory ->
-                                        CategoryListItem(
-                                            coverUrl = Constants.BASE_IMAGE_URL + blogCategory.cover,
-                                            name = blogCategory.name!!,
-                                            isFirst = index == 0,
-                                            isLast = index == categoryViewModel.categoryList.size - 1,
-                                            click = {
-                                                categoryViewModel.currentIndex = index
-                                                showCategoryPopup = false
-                                            }
-                                        )
-                                        if (index != categoryViewModel.categoryList.size - 1) {
-                                            HorizontalDivider(modifier = Modifier.width(200.dp))
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        LazyColumn(modifier = Modifier.padding(top = commonHeaderHeight)) {
             item {
                 if (categoryViewModel.categoryList.isNotEmpty()) {
                     CategoryInfoCard(
@@ -205,21 +131,58 @@ fun CategoryPage(
                 )
             }
         }
+        if (showCategoryPopup) {
+            Surface(
+                color = colorResource(id = R.color.gray_77000000),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .statusBarsPadding()
+                    .padding(0.dp, commonHeaderHeight, 0.dp, 0.dp)
+            ) {
+            }
+        }
+        Column {
+            CommonHeader(
+                leftIcon = R.drawable.ic_arrow_back,
+                rightIcon = R.drawable.ic_more,
+                leftIconClick = { },
+                rightIconClick = { },
+                modifier = Modifier
+                    .background(Color.White)
+                    .padding(12.dp, 0.dp)
+                    .onGloballyPositioned {
+                        with(density) {
+                            commonHeaderHeight = it.size.height.toDp()
+                        }
+                    }
+            ) {
+                Row(modifier = Modifier
+                    .clickable {
+                        showCategoryPopup = !showCategoryPopup
+                    }) {
+                    Text(
+                        text = if (categoryViewModel.currentIndex != -1) categoryViewModel.categoryList[categoryViewModel.currentIndex].name!! else stringResource(
+                            id = R.string.default_category_android
+                        )
+                    )
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_arrow_drop_down),
+                        contentDescription = null
+                    )
+                }
+            }
+            if (showCategoryPopup) {
+                CategoryPopup(dataList = categoryViewModel.categoryList) {
+                    categoryViewModel.currentIndex = it
+                    showCategoryPopup = false
+                }
+            }
+        }
         PullRefreshIndicator(
             refreshing = articleCategoryViewModel.refreshing,
             state = pullRefreshState,
             backgroundColor = Color.White,
             contentColor = colorResource(id = R.color.blue_4285f4)
         )
-    }
-    if (showCategoryPopup) {
-        Surface(
-            color = colorResource(id = R.color.gray_77000000),
-            modifier = Modifier
-                .fillMaxSize()
-                .statusBarsPadding()
-                .padding(0.dp, commonHeaderHeight.dp, 0.dp, 0.dp)
-        ) {
-        }
     }
 }

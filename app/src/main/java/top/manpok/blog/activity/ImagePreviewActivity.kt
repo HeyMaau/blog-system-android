769ba47717e0522a2d21d85c5a5b50ad25e238de
manpok <841294180@163.com>
2024-06-17 19:03:31 +0800

    feat：图片预览页增加序号显示

--- app/src/main/java/top/manpok/blog/activity/ImagePreviewActivity.kt
+++ app/src/main/java/top/manpok/blog/activity/ImagePreviewActivity.kt
@@ -7,18 +7,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowInsetsControllerCompat
 import coil.compose.AsyncImage
 import net.engawapg.lib.zoomable.rememberZoomState
@@ -43,6 +53,10 @@ class ImagePreviewActivity : ComponentActivity() {
         val imageList = intent?.getStringArrayListExtra(INTENT_KEY_IMAGE_LIST)
         val currentIndex = intent?.getIntExtra(INTENT_KEY_CURRENT_INDEX, 0)
         setContent {
+            var currentIndexState by remember {
+                mutableIntStateOf(currentIndex ?: 0)
+            }
+
             Box(
                 modifier = Modifier
                     .background(Color.Black)
@@ -53,6 +67,7 @@ class ImagePreviewActivity : ComponentActivity() {
                         imageList.size
                     }
                     HorizontalPager(state = pagerState, modifier = Modifier.fillMaxSize()) {
+                        currentIndexState = it
                         AsyncImage(
                             model = Constants.BASE_IMAGE_URL + imageList[it],
                             contentDescription = null,
@@ -63,17 +78,29 @@ class ImagePreviewActivity : ComponentActivity() {
                     }
                 }
 
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_arrow_back),
-                    contentDescription = null,
-                    tint = Color.White,
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
+                        .fillMaxWidth()
                         .statusBarsPadding()
-                        .padding(start = 12.dp, top = 4.dp)
-                        .clickable {
-                            finish()
-                        }
-                )
+                        .padding(start = 12.dp, top = 4.dp, end = 12.dp)
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_arrow_back),
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier
+                            .clickable {
+                                finish()
+                            }
+                    )
+                    Text(
+                        text = "${currentIndexState + 1}/${imageList?.size}",
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                }
             }
         }
     }

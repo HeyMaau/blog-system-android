772240379620e5127f397f24b8b85b67968f0590
HeyMaau <841294180@163.com>
2024-06-18 20:16:48 +0800

    feat：添加启动页

--- app/src/main/java/top/manpok/blog/activity/MainActivity.kt
+++ app/src/main/java/top/manpok/blog/activity/MainActivity.kt
@@ -6,18 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import top.manpok.blog.component.BlogScaffold
+import top.manpok.blog.component.LaunchPage
 import top.manpok.blog.ui.theme.BlogSystemAndroidTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var launching by mutableStateOf(true)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            delay(2000)
+            launching = false
+        }
         setContent {
-            BlogSystemAndroidTheme {
-                BlogScaffold()
+            if (launching) {
+                LaunchPage()
+            } else {
+                BlogSystemAndroidTheme {
+                    BlogScaffold()
+                }
             }
         }
     }
---
+++ app/src/main/java/top/manpok/blog/component/LaunchPage.kt
@@ -0,0 +1,51 @@
+package top.manpok.blog.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import top.manpok.blog.R
+
+@Composable
+fun LaunchPage(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .background(Color.White)
+            .fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                bitmap = ImageBitmap.imageResource(id = R.drawable.logo_start),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(
+                text = stringResource(id = R.string.app_name),
+                fontSize = 30.sp,
+                color = colorResource(
+                    id = R.color.blue_4285f4
+                )
+            )
+        }
+    }
+}

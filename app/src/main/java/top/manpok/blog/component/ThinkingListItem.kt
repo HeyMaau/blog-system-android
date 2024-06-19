1bd7c8504a7292f64b7c26135641ca789cb39f73
manpok <841294180@163.com>
2024-06-19 15:02:48 +0800

    feat: 调整首页想法列表点击样式

--- app/src/main/java/top/manpok/blog/component/ThinkingListItem.kt
+++ app/src/main/java/top/manpok/blog/component/ThinkingListItem.kt
@@ -3,12 +3,14 @@ package top.manpok.blog.component
 import android.text.TextUtils
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -28,7 +30,9 @@ fun ThinkingListItem(data: BlogThinking.Data?, click: () -> Unit, modifier: Modi
     Surface(
         color = Color.White,
         shape = RoundedCornerShape(5),
-        modifier = Modifier.clickable(onClick = click)
+        modifier = Modifier.clickable(interactionSource = remember {
+            MutableInteractionSource()
+        }, indication = null, onClick = click)
     ) {
         Column {
             data?.images?.let {

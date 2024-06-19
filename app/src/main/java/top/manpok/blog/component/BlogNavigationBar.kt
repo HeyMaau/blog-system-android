f54bd71399cfe4f55dfa2a6039bd84325c28f11a
manpok <841294180@163.com>
2024-06-19 16:15:38 +0800

    feat: 调整首页底部栏阴影

--- app/src/main/java/top/manpok/blog/component/BlogNavigationBar.kt
+++ app/src/main/java/top/manpok/blog/component/BlogNavigationBar.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
@@ -27,8 +29,7 @@ fun BlogNavigationBar(
     CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
         NavigationBar(
             containerColor = Color.White,
-            contentColor = Color.Red,
-            tonalElevation = 20.dp
+            modifier = Modifier.shadow(elevation = 4.dp)
         ) {
             bottomBarItems.forEachIndexed { index, bottomBarItem ->
                 NavigationBarItem(

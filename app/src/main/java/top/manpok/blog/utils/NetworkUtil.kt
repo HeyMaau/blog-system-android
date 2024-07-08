4c6d5fb0c76ee553cbd68ad13a29f5f97dcd6b5f
manpok <841294180@163.com>
2024-07-08 15:19:04 +0800

    feat: 新增网络工具类

---
+++ app/src/main/java/top/manpok/blog/utils/NetworkUtil.kt
@@ -0,0 +1,19 @@
+package top.manpok.blog.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+class NetworkUtil {
+
+    companion object {
+        fun isNetworkAvailable(context: Context): Boolean {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val network = connectivityManager.activeNetwork ?: return false
+            val networkCapabilities =
+                connectivityManager.getNetworkCapabilities(network) ?: return false
+            return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+        }
+    }
+}

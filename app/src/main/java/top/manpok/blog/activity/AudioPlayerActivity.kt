9bf849d89b1cbac289fa2cdef0de0ba240cdacd3
manpok <841294180@163.com>
2024-09-11 16:35:33 +0800

    feat: 音频播放页-30%，专辑封面圆角

--- app/src/main/java/top/manpok/blog/activity/AudioPlayerActivity.kt
+++ app/src/main/java/top/manpok/blog/activity/AudioPlayerActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +22,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -72,6 +74,7 @@ class AudioPlayerActivity : BaseActivity() {
                         modifier = Modifier
                             .padding(horizontal = 12.dp)
                             .fillMaxWidth()
+                            .clip(RoundedCornerShape(8.dp))
                     )
                     Spacer(modifier = Modifier.height(30.dp))
                     Column(

31985d77956389a824fddb0db7bdf1c5163db3da
HeyMaau <841294180@163.com>
2024-09-26 00:57:49 +0800

    feat: 音频悬浮窗拖动手势及动画

--- app/src/main/java/top/manpok/blog/view/AudioFloatingWindowManager.kt
+++ app/src/main/java/top/manpok/blog/view/AudioFloatingWindowManager.kt
@@ -24,6 +24,8 @@ import top.manpok.blog.viewmodel.GlobalViewModelManager
 object AudioFloatingWindowManager {
 
     private var expandState: ExpandState = ExpandState.Collapse
+    private var screenWidth: Int = 0
+    private var reverse = false
 
     sealed class ExpandState {
         data object Expand : ExpandState()
@@ -36,6 +38,7 @@ object AudioFloatingWindowManager {
             LayoutInflater.from(activity).inflate(R.layout.audio_floating_window, null)
         val icMusic = floatingWindow.findViewById<ImageView>(R.id.ic_music)
         val rotationAnimator = setRotationAnimator(icMusic)
+        screenWidth = activity.resources.displayMetrics.widthPixels
         setListener(activity, floatingWindow, rotationAnimator)
         floatingWindow.y = (activity.resources.displayMetrics.heightPixels / 2).toFloat()
         val layoutParams = ViewGroup.LayoutParams(
@@ -52,6 +55,8 @@ object AudioFloatingWindowManager {
         if (floatingWindow != null) {
             contentView.removeView(floatingWindow)
         }
+        expandState = ExpandState.Collapse
+        reverse = false
     }
 
     private fun setRotationAnimator(view: View): ObjectAnimator {
@@ -68,32 +73,41 @@ object AudioFloatingWindowManager {
         floatingWindow: View,
         rotationAnimator: ObjectAnimator
     ) {
+        val icCtrl = floatingWindow.findViewById<ImageView>(R.id.ic_ctrl)
+
         val collapseRunnable = Runnable {
             setWidthAnimation(activity, floatingWindow, 120F, 50F)
             expandState = ExpandState.Collapse
         }
 
-        floatingWindow.setOnClickListener {
-            if (expandState == ExpandState.Expand) {
-                val intent = Intent(activity, AudioPlayerActivity::class.java)
-                activity.startActivity(intent)
-                return@setOnClickListener
-            }
-            setWidthAnimation(activity, floatingWindow, 50F, 120F)
-            expandState = ExpandState.Expand
-            floatingWindow.postDelayed(collapseRunnable, 2000)
+        val collapseRunnableReverse = Runnable {
+            setWidthAnimationReverse(
+                activity,
+                floatingWindow,
+                120F,
+                50F
+            )
+            expandState = ExpandState.Collapse
         }
+
+
         floatingWindow.setOnTouchListener(object : OnTouchListener {
 
             private var x = 0
             private var y = 0
+            private var actionDownTime = 0L
 
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
                 when (event?.action) {
                     MotionEvent.ACTION_DOWN -> {
                         x = event.rawX.toInt()
                         y = event.rawY.toInt()
-
+                        actionDownTime = System.currentTimeMillis()
+                        removeFloatingWindowRunnable(
+                            floatingWindow,
+                            collapseRunnable,
+                            collapseRunnableReverse
+                        )
                     }
 
                     MotionEvent.ACTION_MOVE -> {
@@ -109,11 +123,55 @@ object AudioFloatingWindowManager {
                         }
                     }
 
+                    MotionEvent.ACTION_UP -> {
+                        if (System.currentTimeMillis() - actionDownTime <= 100) {
+                            onFloatingWindowClick(
+                                activity,
+                                floatingWindow,
+                                collapseRunnable,
+                                collapseRunnableReverse
+                            )
+                            return true
+                        }
+                        val floatingWindowX = floatingWindow.x
+                        if (floatingWindowX < 0 || floatingWindowX <= screenWidth / 2) {
+                            reverseView(false, floatingWindow, icCtrl)
+                            reverse = false
+                            val valueAnimator = ValueAnimator.ofFloat(floatingWindowX, 0F)
+                            valueAnimator.duration = 200
+                            valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+                            valueAnimator.addUpdateListener {
+                                floatingWindow.x = it.animatedValue as Float
+                            }
+                            valueAnimator.start()
+                            if (expandState == ExpandState.Expand) {
+                                floatingWindow.postDelayed(collapseRunnable, 2000)
+                            }
+                        }
+                        if (floatingWindowX > screenWidth / 2) {
+                            reverseView(true, floatingWindow, icCtrl)
+                            reverse = true
+                            val valueAnimator = ValueAnimator.ofFloat(
+                                floatingWindowX,
+                                (screenWidth - floatingWindow.width).toFloat()
+                            )
+                            valueAnimator.duration = 200
+                            valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+                            valueAnimator.addUpdateListener {
+                                floatingWindow.x = it.animatedValue as Float
+                            }
+                            valueAnimator.start()
+                            if (expandState == ExpandState.Expand) {
+                                floatingWindow.postDelayed(collapseRunnableReverse, 2000)
+                            }
+                        }
+                    }
+
                     else -> {
 
                     }
                 }
-                return false
+                return true
             }
         })
         val icClose = floatingWindow.findViewById<ImageView>(R.id.ic_close)
@@ -122,7 +180,6 @@ object AudioFloatingWindowManager {
             removeFloatingWindow(activity)
         }
 
-        val icCtrl = floatingWindow.findViewById<ImageView>(R.id.ic_ctrl)
         icCtrl.setOnClickListener {
             GlobalViewModelManager.audioViewModel.playOrPauseAudio()
             MainScope().launch {
@@ -133,10 +190,49 @@ object AudioFloatingWindowManager {
                 } else {
                     rotationAnimator.resume()
                 }
-                floatingWindow.removeCallbacks(collapseRunnable)
-                floatingWindow.postDelayed(collapseRunnable, 2000)
+                removeFloatingWindowRunnable(
+                    floatingWindow,
+                    collapseRunnable,
+                    collapseRunnableReverse
+                )
+                floatingWindow.postDelayed(
+                    if (reverse) collapseRunnableReverse else collapseRunnable,
+                    2000
+                )
             }
         }
+    }
+
+    private fun reverseView(reverse: Boolean, vararg view: View) {
+        view.forEach {
+            it.rotationY = if (reverse) 180F else 0F
+        }
+    }
+
+    private fun removeFloatingWindowRunnable(view: View, vararg runnable: Runnable) {
+        runnable.forEach {
+            view.removeCallbacks(it)
+        }
+    }
+
+    private fun onFloatingWindowClick(
+        activity: Activity,
+        view: View,
+        collapseRunnable: Runnable,
+        collapseRunnableReverse: Runnable
+    ) {
+        if (expandState == ExpandState.Expand) {
+            val intent = Intent(activity, AudioPlayerActivity::class.java)
+            activity.startActivity(intent)
+            return
+        }
+        if (reverse) {
+            setWidthAnimationReverse(activity, view, 50F, 120F)
+        } else {
+            setWidthAnimation(activity, view, 50F, 120F)
+        }
+        expandState = ExpandState.Expand
+        view.postDelayed(if (reverse) collapseRunnableReverse else collapseRunnable, 2000)
     }
 
     private fun setWidthAnimation(activity: Activity, view: View, start: Float, end: Float) {
@@ -147,7 +243,23 @@ object AudioFloatingWindowManager {
         valueAnimator.duration = 200
         valueAnimator.interpolator = AccelerateDecelerateInterpolator()
         valueAnimator.addUpdateListener {
-            view.layoutParams.width = valueAnimator.animatedValue as Int
+            view.layoutParams.width = it.animatedValue as Int
+            view.requestLayout()
+        }
+        valueAnimator.start()
+    }
+
+    private fun setWidthAnimationReverse(activity: Activity, view: View, start: Float, end: Float) {
+        val valueAnimator = ValueAnimator.ofInt(
+            DensityUtil.dpToPx(activity, start),
+            DensityUtil.dpToPx(activity, end)
+        )
+        valueAnimator.duration = 200
+        valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+        val viewOriginalX = view.x
+        valueAnimator.addUpdateListener {
+            view.layoutParams.width = it.animatedValue as Int
+            view.x = viewOriginalX - (it.animatedValue as Int - DensityUtil.dpToPx(activity, start))
             view.requestLayout()
         }
         valueAnimator.start()

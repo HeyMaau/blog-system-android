f93a5e52cdef49fc5182aa83c11553a6d1a4a080
manpok <841294180@163.com>
2024-10-16 17:31:22 +0800

    feat: 音乐播放器通知添加操作图标

--- app/src/main/java/top/manpok/blog/service/AudioService.kt
+++ app/src/main/java/top/manpok/blog/service/AudioService.kt
@@ -2,6 +2,7 @@ package top.manpok.blog.service
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -19,9 +20,14 @@ import top.manpok.blog.viewmodel.GlobalViewModelManager
 @UnstableApi
 class AudioService : Service() {
 
+    private val ACTION_PLAY = "action_play"
+    private val ACTION_PRE = "action_pre"
+    private val ACTION_NEXT = "action_next"
+
     private val notificationBuilder by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) {
         val application = BaseApplication.getApplication()
         val mediaSession = GlobalViewModelManager.audioViewModel.mediaSession
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
                 Constants.NOTIFICATION_CHANNEL_ID_AUDIO,
@@ -31,6 +37,28 @@ class AudioService : Service() {
             val notificationManager =
                 this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(notificationChannel)
+
+            val playIntent = Intent(this, AudioService::class.java)
+            playIntent.action = ACTION_PLAY
+            val pendingPlayIntent = PendingIntent.getService(
+                this, 1, playIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val preIntent = Intent(this, AudioService::class.java)
+            preIntent.action = ACTION_PRE
+            val pendingPreIntent = PendingIntent.getService(
+                this, 2, preIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val nextIntent = Intent(this, AudioService::class.java)
+            nextIntent.action = ACTION_NEXT
+            val pendingNextIntent = PendingIntent.getService(
+                this, 3, nextIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
             NotificationCompat.Builder(
                 BaseApplication.getApplication(),
                 Constants.NOTIFICATION_CHANNEL_ID_AUDIO
@@ -39,11 +67,35 @@ class AudioService : Service() {
                 .setSmallIcon(R.drawable.ic_pause_floating)
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setOngoing(true)
+                .addAction(R.drawable.ic_skip_previous, "Play previous", pendingPreIntent)
+                .addAction(R.drawable.ic_play_floating, "Play", pendingPlayIntent)
+                .addAction(R.drawable.ic_skip_next, "Play next", pendingNextIntent)
                 .setStyle(
                     MediaStyleNotificationHelper.MediaStyle(mediaSession)
-//                        .setShowActionsInCompactView(0)
+                        .setShowActionsInCompactView(0, 1, 2)
                 )
         } else {
+            val playIntent = Intent(this, AudioService::class.java)
+            playIntent.action = ACTION_PLAY
+            val pendingPlayIntent = PendingIntent.getService(
+                this, 1, playIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val preIntent = Intent(this, AudioService::class.java)
+            preIntent.action = ACTION_PRE
+            val pendingPreIntent = PendingIntent.getService(
+                this, 2, preIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val nextIntent = Intent(this, AudioService::class.java)
+            nextIntent.action = ACTION_NEXT
+            val pendingNextIntent = PendingIntent.getService(
+                this, 3, nextIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
             NotificationCompat.Builder(
                 BaseApplication.getApplication(),
             ).setContentTitle("Havana(feat.Young Thug")
@@ -51,9 +103,12 @@ class AudioService : Service() {
                 .setSmallIcon(R.drawable.ic_pause_floating)
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setOngoing(true)
+                .addAction(R.drawable.ic_skip_previous, "Play previous", pendingPreIntent)
+                .addAction(R.drawable.ic_play_floating, "Play", pendingPlayIntent)
+                .addAction(R.drawable.ic_skip_next, "Play next", pendingNextIntent)
                 .setStyle(
                     MediaStyleNotificationHelper.MediaStyle(mediaSession)
-//                        .setShowActionsInCompactView(0)
+                        .setShowActionsInCompactView(0, 1, 2)
                 )
         }
     }

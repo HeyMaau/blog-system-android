3c5a150694f70b7431e6fb6195707a59742450c7
HeyMaau <841294180@163.com>
2025-01-04 20:37:58 +0800

    feat: 个人信息页面增加昵称

--- app/src/main/java/top/manpok/blog/activity/PersonalInfoActivity.kt
+++ app/src/main/java/top/manpok/blog/activity/PersonalInfoActivity.kt
@@ -12,13 +12,16 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import top.manpok.blog.R
@@ -53,6 +56,12 @@ class PersonalInfoActivity : BaseActivity() {
                         .clip(
                             CircleShape
                         )
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = userViewModel.authorName,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight(500)
                 )
                 Spacer(modifier = Modifier.height(20.dp))
                 AuthorInfoColumn(

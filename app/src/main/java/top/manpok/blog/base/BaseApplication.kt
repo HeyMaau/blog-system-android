c6f0a185a97e7eb978392f8efffa25b75e38b996
manpok <841294180@163.com>
2025-04-14 15:37:25 +0800

    feat: 升级coil及添加referer请求头

--- app/src/main/java/top/manpok/blog/base/BaseApplication.kt
+++ app/src/main/java/top/manpok/blog/base/BaseApplication.kt
@@ -1,6 +1,10 @@
 package top.manpok.blog.base
 
 import android.app.Application
+import coil.Coil
+import coil.ImageLoader
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 
 class BaseApplication : Application() {
 
@@ -20,5 +24,28 @@ class BaseApplication : Application() {
         sDefaultUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler()!!
         Thread.setDefaultUncaughtExceptionHandler(CustomExceptionHandler())
         registerActivityLifecycleCallbacks(BaseActivityLifecycleCallback())
+        initCoilImageLoader()
+    }
+
+    private fun initCoilImageLoader() {
+        val headerInterceptor = Interceptor { chain ->
+            val newRequest = chain.request().newBuilder()
+                .addHeader("referer", "https://m.manpok.top")
+                .build()
+            chain.proceed(newRequest)
+        }
+
+        // 创建 OkHttpClient 并添加拦截器
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(headerInterceptor)
+            .build()
+
+        // 创建 ImageLoader 并设置 OkHttpClient
+        val imageLoader = ImageLoader.Builder(this)
+            .okHttpClient(okHttpClient)
+            .build()
+
+        // 将自定义的 ImageLoader 设置为全局默认
+        Coil.setImageLoader(imageLoader)
     }
 }

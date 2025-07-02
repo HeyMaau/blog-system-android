077dfc5750b0beb005fed7d7113745c42625cfe8
起锚兄 <841294180@163.com>
2025-07-02 16:22:19 +0800

    feat: 通过module的形式引入compose-markdown

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { setUrl("https://jitpack.io") }
     }
 }
 
 rootProject.name = "blog-system-android"
-include(":app")
+include(":app", ":markdowntext")
  
